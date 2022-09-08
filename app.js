05c3bd582184fd356c463a23027056fa582610dd
JuampiAcosta <acostajuan678@gmail.com>
2022-09-07 21:31:17 -0300

    cambie el app.js

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ const mainRoutes=require('./routers/main')
 const app=express();
 app.use(express.static("public"))
 app.use("/",mainRoutes)
-app.listen(3000,()=>{console.log('Servidor funcionando')})
+app.listen(3000,()=>{console.log("http://localhost:3000");
+})
