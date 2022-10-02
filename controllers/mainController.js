51376f268a96ad9836fd06b2de659e290d3e3fbe
JuampiAcosta <acostajuan678@gmail.com>
2022-10-01 21:16:03 -0300

    Se arreglo el metodo sendFile para la vista del home y about

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,4 +1,4 @@
 const path=require('path')
-const controladores={home:(req,res)=>{res.SendFile(path.join(__dirname,"../views/home.html"))}
-,about:(req,res)=>{res.SendFile(path.join(__dirname,"../views/about.html"))},};
+const controladores={home:(req,res)=>{res.sendFile(path.join(__dirname,"../views/home.html"))}
+,about:(req,res)=>{res.sendFile(path.join(__dirname,"../views/about.html"))},};
 module.exports=controladores;
